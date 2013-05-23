d7fd3be6e57fa3554d1257ea751b9d5f09330e6c
Kevin Shu <Kevin@KevintekiMacBook-Pro-2.local>
2013-05-23 19:09:14 +0800

    first

---
+++ Gruntfile.js
@@ -0,0 +1,107 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/*.js','./*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: false,
+        latedef: true,
+        newcap: false,
+        noarg: true,
+        sub: false,
+        undef: true,
+        unused: true,
+        boss: false,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+        'css/main.css': [
+            'sass/reset.sass',
+            'sass/main.sass'
+          ]
+        }
+      }
+    },
+    // qunit: {
+    //   files: ['test/**/*.html']
+    // },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: ['jades/*', 'sass/*'],
+        tasks: ['jade', 'sass']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+    jade: {
+      html: {
+        files: {
+          './': ['jades/*.jade']
+        },
+        options: {
+          client: false
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-jade');
+
+  // Default task.
+  grunt.registerTask('default', ['concat', 'jshint', 'uglify', 'jade', 'sass', 'watch']);
+
+};
